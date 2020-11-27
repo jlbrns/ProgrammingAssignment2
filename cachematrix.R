83117b70c3b5ba2126b47cacc934ceb890050d6e
jlbrns <jhonluisbernas@gmail.com>
2020-11-28 06:20:09 +0800

    uploading Programming Assignment 2 lexical scoping for Coursera: R Programming

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions are intended to cache the inverse of a matrix.
+## I used the solve() function in computing the inverse.
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse. I largely followed the example given re: mean
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse_data <- NULL
+  set <- function(y) {
+    x <<- y
+    inverse_data <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) inverse_data <<- inverse
+  getInverse <- function() inverse_data
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
-
-
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix. Here, I applied the solve() function as suggested in the assignment.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse_data <- x$getInverse()
+  if (!is.null(inverse_data)) {
+    message("getting cached data")
+    return(inverse_data)
+  }
+  value <- x$get()
+  inverse_data <- solve(value, ...)
+  x$setInverse(inverse_data)
+  inverse_data
 }
+
+## Here, I used the test matrices given on one of the discussions to make sure that I'm getting the correct result.
+
+mat_data <- makeCacheMatrix(matrix(c(1/2,-1/4,-1,3/4), nrow = 2, ncol = 2))
+mat_data$get()
+cacheSolve(mat_data)
+
+mat_data <- makeCacheMatrix(matrix(c(5/8, -1/8, -7/8, 3/8), nrow = 2, ncol = 2))
+mat_data$get()
+cacheSolve(mat_data)
+
